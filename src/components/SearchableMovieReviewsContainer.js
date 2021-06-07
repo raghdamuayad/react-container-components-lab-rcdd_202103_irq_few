7896284858a008b3e8a55ed0c7fb4feafb96054f
Raghda  Muayad <therose000777@gmail.com>
2021-06-07 08:41:58 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,39 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+
+
+class SearchableMovieReviewsContainer extends Component {
+constructor(){
+super();
+
+this.state = {
+searchTerm: '', reviews: []
+  }
+  }
+
+
+submitEvent(event) {
+event.preventDefault();
+fetch(URL.concat(this.state.searchTerm))
+.then(resp => resp.json())
+.then(resp => this.setState({ reviews: resp.results }));
+  }
+
+render(){
+return(
+<div className="searchable-movie-reviews">
+<form>
+<h3>The Searched Movie Reviews:</h3>
+</form>
+<MovieReviews reviews={this.state.reviews} />
+</div>
+  );
+  }
+  }
+
+export default SearchableMovieReviewsContainer;
