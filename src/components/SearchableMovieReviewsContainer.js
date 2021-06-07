524d68eb9e8b1d38ffdb9c2b12eef83e9d66aa3a
Raghda  Muayad <therose000777@gmail.com>
2021-06-07 08:46:54 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const BASE_URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-                 + `api-key=${NYT_API_KEY}&query=`;
+const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
+            + `api-key=${NYT_API_KEY}&query=`;
 
 
 
