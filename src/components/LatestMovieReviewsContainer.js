9bc4450c57eaf1611f02d144df7ff313dde3449a
Raghda  Muayad <therose000777@gmail.com>
2021-06-07 12:32:47 +0000

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+constructor(){
+super();
+this.state = {
+reviews: []
+  }
+  }
+componentWillMount(){
+    fetch(URL)
+      .then(resp =>  resp.json)
+      .then(resp => this.setState({ reviews: resp.results }))
+  }
+
+  render(){
+  return(
+  <div className="latest-movie-reviews">
+  <h3>The Latest Reviews:</h3>
+  <MovieReviews reviews={this.state.reviews} />
+  </div>
+  );
+  }
+ }
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,38 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = '55354e1e031a4ddeb4aa0f48b1a13a54';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+                 + `api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+
+class SearchableMovieReviewsContainer extends Component {
+
+  state = {
+  searchTerm: '',
+  reviews: []
+  }
+  handleSearch = event =>this.setState({searchTerm: event.target.value});
+  handleSubmit = event => {
+  event.preventDefault();
+  fetch(URL.concat(this.state.searchTerm)).then(res => res.json()).then(res => this.setState({ searchTerm: res.results}));
+  }
+  render() {
+  return (
+  <div className="searchable-movie-reviews">
+  <form onSubmit={ this.handleSubmit }>
+  <label htmlFor='search-input'>Search Movie Reviews</label>
+  <input
+  id='search-input'
+  type="text"
+  onChange={ this.handleSearch } />
+  <button type="submit">Submit</button>
+  </form>
+  { this.state.reviews.length > 0 && <h2>Search for Movies:</h2> }
+  <MovieReviews reviews={ this.state.reviews } />
+  </div>
+  );
+  }
+  }
+
+  export default SearchableMovieReviewsContainer;
